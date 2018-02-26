f1171edb13e3fa923ed5bdb835783762427c4520
fsoc <8392143a@gmail.com>
2018-02-26 03:32:53 +0100

    Return a function, improve code quality.

--- scripts/getFriends.js
+++ scripts/getFriends.js
@@ -1,28 +1,30 @@
-const URL = 'http://api.steampowered.com/'
+const URL = 'http://api.steampowered.com/';
 
-const request = require('request')
+const request = require('request');
+const debug = require('debug')('app:friends');
 
-const log = require('../components/logger')
+const getFriends = module.exports = (id) => {
+  debug(`get for ${id}`);
 
-const getFriends = module.exports = id => {
-  let url = `${URL}ISteamUser/GetFriendList/v0001/` +
-    `?key=${global.API_KEY}&steamid=${id}&relationship=friend`
+  const url = `${URL}ISteamUser/GetFriendList/v0001/` +
+    `?key=${global.API_KEY}&steamid=${id}&relationship=friend`;
 
   return new Promise((resolve, reject) => {
     request(url, (err, res, body) => {
-      if (err) 
-        return reject(err)
+      debug(`steam api response (${res.statusCode})`);
+
+      if (err) { throw err; }
 
       try {
         // This will cause an exception if key invalid or profile private.
-        let friends = JSON.parse(body).friendslist.friends
-        log.debug(`got friend list of user ${id}`)
+        const friends = JSON.parse(body).friendslist.friends;
+        debug(`got friend list of user ${id}`);
 
-        resolve(friends)
+        resolve(friends);
       } catch(e) {
-        log.warn(`couldn't get friendslist, you should probably abort`)
-        reject(e)
+        debug(`couldn't get friendslist, you should probably abort`);
+        reject(e);
       }
-    })
+    });
   })
 }
